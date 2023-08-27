a32ab7ff6e23548d41525630c30cf8bb4013bf70
MrAtlas <atlastech365@gmail.com>
2023-08-27 13:23:15 +0200

    Updated fucntions
    I updated functions generateCard and modal, so to clean up a bit

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -44,38 +44,43 @@ divSearchContainer.insertAdjacentHTML('afterend', searchHtml)
 
 
 //generateCard Will generate a card with the given parameters and then return that card
-function generateCard(picture, firstName, lastName, email, city, state){
+function generateCard(object){
+
     const userCardHtml = `
     <div class="card">
     <div class="card-img-container">
-        <img class="card-img" src=${picture} alt=profile picture>
+        <img class="card-img" src=${object.picture.medium} alt=profile picture>
     </div>
     <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-        <p class="card-text">${email}</p>
-        <p class="card-text cap">${city}, ${state}</p>
+        <h3 id="name" class="card-name cap">${object.name.first} ${object.name.last}</h3>
+        <p class="card-text">${object.email}</p>
+        <p class="card-text cap">${object.location.city}, ${object.location.state}</p>
     </div>
     </div>
 `;
-    return userCardHtml;
+    divPhotoGalleryContainer.insertAdjacentHTML('beforeend', userCardHtml);
 }
 
 //For modal
 //modal as the generateCard will create a modal (from index.html) with the parameters given 
-function modal(picture, firstName, lastName, email, city, phone, street, state, postal, dob){
+function modal(object){
+
+    const dob = new Date(object.dob.date);
+    const formattedDob = `${dob.getMonth() + 1}/${dob.getDate()}/${dob.getFullYear()}`;
+    
     const modalHtml = `
         <div class="modal-container">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src=${picture} alt="profile picture">
-                <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
-                <p class="modal-text">${email}</p>
-                <p class="modal-text cap">${city}</p>
+                <img class="modal-img" src=${object.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${object.name.first} ${object.name.last}</h3>
+                <p class="modal-text">${object.email}</p>
+                <p class="modal-text cap">${object.location.city}</p>
                 <hr>
-                <p class="modal-text">${phone}</p>
-                <p class="modal-text">${street}, ${city}, ${state}, ${postal}</p>
-                <p class="modal-text">Birthday: ${dob}</p>
+                <p class="modal-text">${object.phone}</p>
+                <p class="modal-text">${object.location.street.number} ${object.location.street.name}, ${object.location.city}, ${object.location.state}, ${object.location.postcode}</p>
+                <p class="modal-text">Birthday: ${formattedDob}</p>
             </div>
             <div class="modal-btn-container">
                 <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
@@ -84,28 +89,59 @@ function modal(picture, firstName, lastName, email, city, phone, street, state, 
         </div>
     `;
     //divPhotoGalleryContainer.insertAdjacentElement('afterend', modalHtml);
-    return modalHtml;
+    divPhotoGalleryContainer.insertAdjacentHTML('afterend', modalHtml);
 }
 
-function getModal(object){
-    const dob = new Date(object.dob.date);
+function prevModal(prevUser){
+    const currentModal = document.querySelector('.modal');
+
+    const dob = new Date(prevUser.dob.date);
     const formattedDob = `${dob.getMonth() + 1}/${dob.getDate()}/${dob.getFullYear()}`;
 
-    const modalUserHtml = modal(
-        object.picture.large,
-        object.name.first,
-        object.name.last,
-        object.email,
-        object.location.city,
-        object.phone,
-        `${object.location.street.number} ${object.location.street.name}`,
-        object.location.state,
-        object.location.postcode,
-        formattedDob
-    );
-
-    divPhotoGalleryContainer.insertAdjacentHTML('afterend', modalUserHtml);
+    currentModal.innerHTML = `
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src=${prevUser.picture.large} alt="profile picture">
+            <h3 id="name" class="modal-name cap">${prevUser.name.first} ${prevUser.name.last}</h3>
+            <p class="modal-text">${prevUser.email}</p>
+            <p class="modal-text cap">${prevUser.location.city}</p>
+            <hr>
+            <p class="modal-text">${prevUser.phone}</p>
+            <p class="modal-text">${prevUser.location.street.number} ${prevUser.location.street.name}, ${prevUser.location.city}, ${prevUser.location.state}, ${prevUser.location.postcode}</p>
+            <p class="modal-text">Birthday: ${formattedDob}</p>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    `;
 }
+
+function nextModal(nextUser){
+    const currentModal = document.querySelector('.modal');
+
+    const dob = new Date(nextUser.dob.date);
+    const formattedDob = `${dob.getMonth() + 1}/${dob.getDate()}/${dob.getFullYear()}`;
+
+    currentModal.innerHTML = `
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src=${nextUser.picture.large} alt="profile picture">
+            <h3 id="name" class="modal-name cap">${nextUser.name.first} ${nextUser.name.last}</h3>
+            <p class="modal-text">${nextUser.email}</p>
+            <p class="modal-text cap">${nextUser.location.city}</p>
+            <hr>
+            <p class="modal-text">${nextUser.phone}</p>
+            <p class="modal-text">${nextUser.location.street.number} ${nextUser.location.street.name}, ${nextUser.location.city}, ${nextUser.location.state}, ${nextUser.location.postcode}</p>
+            <p class="modal-text">Birthday: ${formattedDob}</p>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    `;
+}
+
 
 function closeModal(){
     const closeModal = document.getElementById('modal-close-btn');
@@ -117,47 +153,46 @@ function closeModal(){
 let people = [];
 
 getUsers().then(data => {
+    //this will copy the data from the fetch into the people array
     people.push(...data.results);
 
+    //this will generate a card for each user object in people array
     for(let i = 0; i < people.length; i++){
-        const userCardHtml = generateCard(
-            people[i].picture.medium,
-            people[i].name.first,
-            people[i].name.last,
-            people[i].email,
-            people[i].location.city,
-            people[i].location.state
-        )
-        divPhotoGalleryContainer.insertAdjacentHTML('beforeend', userCardHtml);
+        generateCard(people[i])
     }
 
+    //select all the cards generated
     const cards = divPhotoGalleryContainer.querySelectorAll('.card');
 
+    //Add an event listener to each of the card and get their index
     cards.forEach((card, index) => {
         card.addEventListener('click', () => {
             const userData = people[index];
 
-            getModal(userData);
+            //this will create a modal with the userData of index clicked,
+            //basically when a card is clicked it will call modal with that object
+            modal(userData);
 
             closeModal();
 
+            /**
+             * First selecting the next button and prev button
+             */
             const modalPrev = document.getElementById('modal-prev');
             const modalNext = document.getElementById('modal-next');
 
             let currentIndex = index;
 
             modalPrev.addEventListener('click', () => {
-                currentIndex = (currentIndex - 1 + people.length) % people.length;
-                closeModal();
+                currentIndex = index - 1;
                 const prevUser = people[currentIndex];
-                getModal(prevUser);
+                prevModal(prevUser);
             });
 
             modalNext.addEventListener('click', () => {
                 currentIndex = index + 1;
-                closeModal();
                 const nextUser = people[currentIndex];
-                getModal(nextUser);
+                nextModal(nextUser);
             });
         });
     });
@@ -178,15 +213,8 @@ getUsers().then(data => {
         divPhotoGalleryContainer.innerHTML = '';
 
         filteredUsers.forEach(user => {
-            const userCardHtml = generateCard(
-                user.picture.medium,
-                user.name.first,
-                user.name.last,
-                user.email,
-                user.location.city,
-                user.location.state
-            )
-            divPhotoGalleryContainer.insertAdjacentHTML('beforeend', userCardHtml);
+            generateCard(user);
+            //divPhotoGalleryContainer.insertAdjacentHTML('beforeend', userCardHtml);
         });
     });
 })
